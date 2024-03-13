bb42f768593f5f0d1ed4bc54054d23eede541c83
DaveHagenauer <david.hagenauer05@gmail.com>
2024-03-13 08:50:57 +0100

    Projekt angelegt, MyGame erstellt, Manager class erstellt

---
+++ Assets/MyGame/Scripts/Manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //array von wörtern 
+    //Methode die bei drücken von Start button ausgeführt wird und randome einen der begriffe auswählt
+    // alphabetische buttons zum buchstaben probieren
+    // methode die mit abfragt ob der richtige buchstabe gedrückt wurde
+    // Methode die aufgerufen wird wenn ein falscher buchstabe gedrückt wurde, galgen zeichnen
+    // methode die aufgerufen wird wenn ein richtiger Buchstabe gedrückt wurde, buchstaben eintragen
+    // Verloren Methode und Gewonnen Methode
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
